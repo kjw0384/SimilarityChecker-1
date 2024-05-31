6b49d2b200d317576891e2899eff791a6edc00a0
kjw0384 <kjw0384@gmail.com>
2024-05-31 09:10:15 +0900

    길이 2배이상 case - refactor

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-TEST(SimilarityCheckerTest, samelength) {
+TEST(SimilarityCheckerTest, lengthSame) {
 	SimilarityChecker sc;
 	int nScore = sc.getLengthScore("ASD", "DSA");
 	EXPECT_EQ(nScore, 60);
 }
 
-TEST(SimilarityCheckerTest, difflength2) {
+TEST(SimilarityCheckerTest, lengthGE2Legnth) {
 	SimilarityChecker sc;
 	int nScore = sc.getLengthScore("A", "BB");
 	EXPECT_EQ(nScore, 0);

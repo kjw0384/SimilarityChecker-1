a17adb6e10fb0f59e34a7b1e1407b5ad3670097c
kjw0384 <kjw0384@gmail.com>
2024-05-31 09:41:11 +0900

    길이 2배 미만 case2 - refactor  - case 표준화 : 해당 식은 수식 상 길이 차이 없을 시 60점, 길이 차가 2배 이상일 시 0 이하 점수가 나오도록 되어있음.

--- Project12/SimilarityChecker.cpp
+++ Project12/SimilarityChecker.cpp
@@ -5,20 +5,9 @@ using namespace std;
 class SimilarityChecker {
 public:
 	int getLengthScore(const string input1, const string input2) {
-		int length1 = input1.length();
-		int length2 = input2.length();
-
-		if (length1 == length2)
-			return 60;
-		else if (length1 >= 2 * length2 ||
-			length2 >= 2 * length1)
-			return 0;
-		else {
-			double A = (length1 > length2) ? length1 : length2;
-			double B = (length1 > length2) ? length2 : length1;
-			double Gap = A - B;
-			return (int)((1 - (Gap / B)) * 60);
-		}
-		return 0;
+		double A = (input1.length() > input2.length()) ? input1.length() : input2.length();
+		double B = (input1.length() > input2.length()) ? input2.length() : input1.length();
+		double Gap = A - B;
+		return max((int)((1 - (Gap / B)) * 60), 0);
 	}
 };
